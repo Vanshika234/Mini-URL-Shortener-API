ffc1ea25205785bdc0db0f2d83b887ced7e7062d
Vanshika Kushwaha <119745779+Vanshika234@users.noreply.github.com>
2025-07-11 16:07:41 +0530

    Add files via upload

--- url.js
+++ url.js
@@ -1,32 +1,9 @@
-const shortid = require("shortid");
-const URL = require('../models/url');
+const express = require("express");
+const { handleGenerateNewShortURL, handleGetAnalytics } = require("../controllers/url");
+const router = express.Router();
 
-async function handleGenerateNewShortURL(req, res) {
-    const body = req.body;
-    if (!body.url) return res.status(400).json({ error: 'url is required' });
+router.post("/", handleGenerateNewShortURL);
 
-    const shortID = shortid();
-    await URL.create({
-        shortID: shortID,
-        redirectURL: body.url,
-        visitHistory: [],
-    });
+router.get('/analytic/:shortId', handleGetAnalytics);
 
-    return res.json({ id: shortID });
-}
-
-async function handleGetAnalytics(req, res) {
-    const shortId = req.params.shortId;
-    const result = await URL.findOne({ shortId });
-    return res.json({ 
-        totalClicks: result.visitHistory.length, 
-        analytics: result.visitHistory,
-
-    });
-
-}
-
-module.exports = {
-    handleGenerateNewShortURL,
-    handleGetAnalytics,
-};
+module.exports = router;
